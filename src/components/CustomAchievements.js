47255aaf5bd6bae8dadce65814fb006c314dc14c
Thomas Horster <thomas.horster@trivago.com>
2018-12-06 20:22:14 +0100

    Achievements, Leaderboard and Players added, working on subroutes

--- src/components/CustomAchievements.js
+++ src/components/CustomAchievements.js
@@ -6,30 +6,26 @@ export default class CustomAchievements extends Component {
 
     render() {
         return (
-                <section className="container with-title custom-achievements">
-                    <h2 className="title">Custom Achievements</h2>
-                    {/*<section className="container with-title">*/}
-                        <div className="messages">
-                            <div className="message -left">
-                                <i className="bcrikko">
-                                </i>
-                                <div className="balloon from-left">
-                                    <p>Hello, would you like to reward someone? Please enter an achievement and submit
-                                        :)</p>
-                                </div>
-                            </div>
+            <section className="container with-title balloon">
+                <h2 className="title">Custom Achievements</h2>
+                <div className="messages">
+                    <div className="message -left">
+                        <i className="bcrikko">
+                        </i>
+                        <div className="balloon from-left">
+                           <p>Hello, would you like to reward someone? Please enter an achievement and submit :)</p>
                         </div>
-                        <form autoComplete="on">
-                            <div className="field">
-                                <label htmlFor="name_field" className="formTitle">Achievement unlocked:
-                                    <input type="text" id="name_field" className="input" autoFocus/>
-                                </label>
-                            </div>
-                            <input type="submit" value="Unlock" className="btn is-primary"/>
-                        </form>
-                    {/*</section>*/}
-                </section>
-
+                    </div>
+                </div>
+                <form autoComplete="on">
+                    <div className="field">
+                    <label htmlFor="name_field" className="formTitle">Achievement unlocked:
+                    <input type="text" id="name_field" className="input" autoFocus />
+                    </label>
+                    </div>
+                    <input type="submit" value = "Unlock" className="customachievements btn is-primary"/>
+                </form>
+            </section>
         );
     }
 };
