77edffbea336d0980132e21d9c0b1efe19e9597b
bayfera <bezawit.ayfera@trivago.com>
2018-12-06 20:14:41 +0100

    adjusted some css

--- src/components/CustomAchievements.js
+++ src/components/CustomAchievements.js
@@ -6,27 +6,30 @@ export default class CustomAchievements extends Component {
 
     render() {
         return (
-            <section className="container with-title balloon custom-achievements">
-                <h2 className="title">Custom Achievements</h2>
-                <div className="messages">
-                    <div className="message -left">
-                        <i className="bcrikko">
-                        </i>
-                        <div className="balloon from-left">
-                           <p>Hello, would you like to reward someone? Please enter an achievement and submit :)</p>
+                <section className="container with-title custom-achievements">
+                    <h2 className="title">Custom Achievements</h2>
+                    {/*<section className="container with-title">*/}
+                        <div className="messages">
+                            <div className="message -left">
+                                <i className="bcrikko">
+                                </i>
+                                <div className="balloon from-left">
+                                    <p>Hello, would you like to reward someone? Please enter an achievement and submit
+                                        :)</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <form autoComplete="on">
-                    <div className="field">
-                    <label htmlFor="name_field" className="formTitle">Achievement unlocked:
-                    <input type="text" id="name_field" className="input" autoFocus />
-                    </label>
-                    </div>
-                    <input type="submit" value = "Unlock" className="btn is-primary"/>
-                </form>
+                        <form autoComplete="on">
+                            <div className="field">
+                                <label htmlFor="name_field" className="formTitle">Achievement unlocked:
+                                    <input type="text" id="name_field" className="input" autoFocus/>
+                                </label>
+                            </div>
+                            <input type="submit" value="Unlock" className="btn is-primary"/>
+                        </form>
+                    {/*</section>*/}
+                </section>
 
-            </section>
         );
     }
 };
